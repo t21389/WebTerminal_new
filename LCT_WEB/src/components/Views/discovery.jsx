b9c6c926bec4cae2f80cce1f80a73cd58f46c54f
noopur <noopur2309@yahoo.in>
2019-04-24 09:42:03 +0530

    new

--- LCT_WEB/src/components/Views/discovery.jsx
+++ LCT_WEB/src/components/Views/discovery.jsx
@@ -1,28 +1,67 @@
 import React, { Component } from "react";
-import Window from "./window";
 import "./../../css/discovery.css";
+import Window from "./window";
 class Discovery extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      show: true
+      value: "select",
+      shown: true,
+      show: false
     };
     this.toggleDiv = this.toggleDiv.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      shown: !this.state.shown
+    });
+  }
+
+  onChange(e) {
+    this.setState({
+      value: e.target.value
+    });
   }
   toggleDiv = () => {
     const { show } = this.state;
     this.setState({ show: !show });
   };
-
   render() {
-    return (
-      <div className="App-intro">
-        <div class="header">
-          <h1>cdot</h1>
-        </div>
+    var shown = {
+      display: this.state.shown ? "block" : "none"
+    };
 
-        <button onClick={this.toggleDiv}>Local</button>
-        {this.state.show && <Window />}
+    var hidden = {
+      display: this.state.shown ? "none" : "block"
+    };
+    return (
+      <div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-3 col-md-2" />
+            <div class="col-lg-6 col-md-8 login-box">
+              <div class="col-lg-12 login-key">
+                <i class="fa fa-key" aria-hidden="true" />
+              </div>
+              <div class="col-lg-12 login-title">
+                Optical Transport 123 Controller
+              </div>
+
+              <div class="row">
+                <div class="col-sm-3 col-md-6 col-lg-4">
+                  <button onClick={this.toggleDiv}>Local</button>
+                </div>
+              </div>
+
+              <div class="col-lg-12 login-form">
+                {this.state.show && <Window />}
+              </div>
+              <div class="col-lg-3 col-md-2" />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- LCT_WEB/src/components/Views/window.jsx
+++ LCT_WEB/src/components/Views/window.jsx
@@ -1,8 +1,63 @@
 import React, { Component } from "react";
-
+import "./../../css/discovery.css";
 class Window extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: "select"
+    };
+  }
+
+  onChange(e) {
+    this.setState({
+      value: e.target.value
+    });
+  }
   render() {
-    return <div className="intro">this is div</div>;
+    return (
+      <div>
+        <div class="col-lg-12 login-form">
+          <form>
+            <div class="form-group">
+              <label htmlFor="select1">Interface Name </label>
+              <select
+                value={this.state.value}
+                onChange={this.onChange.bind(this)}
+                className="form-control"
+              >
+                <option value="select">eth0</option>
+                <option value="First">eth1</option>
+                <option value="Second">eth2</option>
+                <option value="Third">eth3</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label htmlFor="select1">IP</label>
+              <select
+                value={this.state.value}
+                onChange={this.onChange.bind(this)}
+                className="form-control"
+              >
+                <option value="select">10.5.0.10</option>
+                <option value="First">10.5.0.11</option>
+                <option value="Second">10.5.0.12</option>
+                <option value="Third">10.5.0.13</option>
+              </select>
+            </div>
+
+            <div class="col-lg-12 loginbttm">
+              <div class="col-lg-6 login-btm login-text" />
+              <div class="col-lg-6 login-btm login-button">
+                <button type="submit" class="btn btn-outline-primary">
+                  LOGIN
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
