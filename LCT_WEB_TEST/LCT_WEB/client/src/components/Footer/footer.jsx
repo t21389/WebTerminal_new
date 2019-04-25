0eeb14d401326d7659529b7d892fd182ccc9134b
noopur <noopur2309@yahoo.in>
2019-04-25 11:55:33 +0530

    n

---
+++ LCT_WEB_TEST/LCT_WEB/client/src/components/Footer/footer.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "./../../css/footer.css";
+
+class Footer extends Component {
+    constructor(props) {
+        super(props);
+    
+      }
+  render() {
+    const themeBgColor = {
+        background:  this.props.mainThemeColor
+    };
+
+    return (
+        
+
+<div class="footer">
+
+<p>© Copyright <strong>C-DOT</strong>. All Rights Reserved
+</p>
+   </div>
+    );
+  }
+}
+
+export default Footer;
