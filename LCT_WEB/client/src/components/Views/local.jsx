5ce918646398afc1042c33cf93b3f9860cf9100b
noopur <noopur2309@yahoo.in>
2019-04-26 16:48:29 +0530

    Local Interface Event Added

--- LCT_WEB/client/src/components/Views/local.jsx
+++ LCT_WEB/client/src/components/Views/local.jsx
@@ -5,16 +5,24 @@ class Local extends React.Component {
     super(props);
 
     this.state = {
-      value: "select"
+      value: "select",
+      isChecked: props.isChecked || false
     };
+    this.handleChange = this.handleChange.bind(this);
   }
 
+  handleChange() {
+    this.setState({ isChecked: !this.state.isChecked });
+  }
   onChange(e) {
     this.setState({
       value: e.target.value
     });
   }
   render() {
+    var interfaceJson = this.props.interfaceJson;
+    console.log("interfaceJson final: ", interfaceJson[2]);
+
     return (
       <div>
         <div class="col-lg-12 login-form">
@@ -26,12 +34,24 @@ class Local extends React.Component {
                 onChange={this.onChange.bind(this)}
                 className="form-control"
               >
-                <option value="select">eth0</option>
+                <option value="select">as</option>
                 <option value="First">eth1</option>
                 <option value="Second">eth2</option>
                 <option value="Third">eth3</option>
               </select>
             </div>
+            <div class="form-group">
+              <h6>SDN Enabled</h6>
+              <label className="switch">
+                <input
+                  type="checkbox"
+                  value={this.state.isChecked}
+                  onChange={this.handleChange}
+                />
+                <div className="slider" />
+              </label>
+            </div>
+
             <div class="col-lg-12 loginbttm">
               <div class="col-lg-12 login-btm login-button">
                 <button type="submit" class="btn btn-outline-primary">
--- LCT_WEB/client/src/components/Views/modal.jsx
+++ LCT_WEB/client/src/components/Views/modal.jsx
@@ -2,11 +2,17 @@ import React, { Component } from "react";
 import "./../../css/modal.css";
 
 class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messgae: "MAIN Component"
+    };
+  }
   render() {
     if (!this.props.showM) {
       return null;
     }
-    return <div>{this.props.children}</div>;
+    return <div>{this.props.message}</div>;
   }
 }
 
